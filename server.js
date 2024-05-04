ede5e38560c650eaada62249562ec79dca979c37
MKaige <144488413+MKaige@users.noreply.github.com>
2024-05-04 11:42:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/random.routes');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use('/recipes', recipeRouter);
 app.use('/ingredients', ingredientRouter);
 app.use('/fullRecipes', fullRecipesRouter);
